2907a60c045c2d82a82b788ca347e9a0c474b262
mahmoud08sakr <93199475+mahmoud08sakr@users.noreply.github.com>
2024-12-01 17:27:04 +0200

    mongoo connection update

--- database/connection.js
+++ database/connection.js
@@ -1,15 +1,14 @@
 import { connect } from "mongoose";
 const connectDB = async () => {
-    console.log(process.env.MONGO_URI); 
-    try {
-        await connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+
+    await connect(process.env.MONGO_URI).then(() => {
         console.log("MongoDB connected");
-    } catch (error) {
+
+    }).catch((error) => {
         console.log(error);
-        process.exit(1);
-    }
-};
+    })
+
+
+}
+
 export default connectDB;
